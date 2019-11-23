d60886e77900d2d5a479e5adef8de290bdacf0b8
Andrew Luchuk <andrew.luchuk@outlook.com>
2019-11-23 17:35:37 -0500

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,30 @@
 class Employee
 
+    attr_reader :name, :salary
+    attr_accessor :manager
+
+    @@all = []
+
+    def initialize(name, salary, manager)
+        @name = name
+        @salary = salary
+        @manager = manager
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def tax_bracket
+        @@all.select do |employee| 
+            employee.salary <= self.salary+1000 and employee.salary >= self.salary-1000
+        end
+    end
+
+    def self.paid_over(amount)
+        self.all.select {|employee| employee.salary > amount}
+    end
+
 end
--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,39 @@
 class Manager
 
+    attr_reader :name
+    attr_accessor :department, :age
+
+    @@all = []
+
+    def initialize(name, age, department)
+        @name = name
+        @age = age
+        @department = department
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def employees
+        Employee.all.select {|employee| employee.manager == self}
+    end
+
+    def hire_employee(name, salary)
+        Employee.new(name, salary, self)
+    end
+
+    def self.all_departments
+        departments = self.all.map {|manager| manager.department}
+        departments.uniq
+    end
+
+    def self.average_age
+        managers = self.all
+        total = managers.reduce(0) {|total, manager| total += manager.age}
+        total / managers.count
+    end
+
 end
