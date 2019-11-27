28903d31ccdebf537c07b5c34b5b7b99d717baa0
github username <github email address>
2019-11-26 22:37:34 -0500

    Done.

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,32 @@
 class Manager
+  attr_accessor :department
+  attr_reader :name, :age
+    @@all = []
+    def initialize(name, age, department)
+        @name = name
+        @age = age
+        @department = department
+        @@all << self
+    end
 
+    def self.all
+      @@all
+    end
+
+    def hire_employee(name, salary)
+       Employee.new(name, salary, self)
+    end
+
+    def all_departments
+        Manager.all.map do |manager|
+        manager.department
+      end
+    end
+
+    def average_age
+    age_array = Manager.all.map do |manager|
+      manager.age
+    end
+    age_array.inject(0){|sum,x| sum + x }/ Manager.all.count
+    end
 end
--- run.rb
+++ run.rb
@@ -5,6 +5,23 @@ require 'pry'
 
 #Test your code here
 
+###### Mangager #######
+
+mike = Manager.new("Mike", 36, "Shoes")
+steven = Manager.new("James", 45, "Appliances")
+terrance = Manager.new("Terrance", 28, "Electronics")
+
+
+######Employees####
+
+kevin = Employee.new("Kevin", 35000, terrance)
+doug = Employee.new("Doug", 33000, steven)
+navarre = Employee.new("Navarre", 26000, mike)
+jasmine = Employee.new("Jasmine", 34500, mike)
+isaiah = Employee.new("Isaiah", 35000, steven)
+kyle = Employee.new("Kyle", 45000, terrance)
+victoria = Employee.new("Victoria", 36000, mike)
+brian = Employee.new("Brian", 30000, terrance)
 
 binding.pry
 puts "done"
